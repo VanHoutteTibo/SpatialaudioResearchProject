e78d4d3231b00d830d9fc2e884f2120cad842b3d
VanHoutteTibo <tibo.van.houtte@student.howest.be>
2022-01-17 19:22:46 +0100

    added gtoundplane and sky

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 import { Suspense } from 'react/cjs/react.production.min'
@@ -13,7 +13,14 @@ import Car from './Objects/PlayerCar';
 
 function App() {
 
-
+  function GroundPlane() {
+    return (
+      <mesh receiveShadow rotation={[-1.555, 0, 0]} position={[0, 0, 0]}>
+        <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+        <meshStandardMaterial attach="material" color="green" />
+      </mesh>
+    );
+  }
 
   return (
     <>
@@ -21,10 +28,12 @@ function App() {
     <div className="App">
       <Canvas style={{height: window.innerHeight}}>
         <Suspense fallback={null}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
           <Car />
           <OrbitControls />
-          <Environment preset="sunset" background />
-          
+          <Sky  distance={450000} sunPosition={[5, 1, 8]}  azimuth={0.25}/>
+          <GroundPlane/>
         </Suspense>
       </Canvas>
     </div>
